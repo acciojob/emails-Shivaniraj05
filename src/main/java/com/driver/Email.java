f49c6c8ffb2e71e8be3deb62c815c5592b85b075
Shivani <shivaniraj7191@gmail.com>
2022-12-30 12:23:42 +0530

    assignment

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -18,12 +18,50 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (oldPassword.equals(password)) {
+            if (isValid(newPassword)) {
+                System.out.println("Password changed successfully");
+                this.password = newPassword;
+            } else {
+                System.out.println("The new password is not valid");
+            }
+        } else {
+            System.out.println("The given password does not match current password!");
+        }
+    }
+
+    private Boolean isValid(String password){
+        Boolean upperLetter = false;
+        Boolean lowerLetter = false;
+        Boolean digit = false;
+        Boolean specialChar = false;
+        if(password.length() < 8){
+            return false;
+        }
+        for(int i=0; i<password.length();i++){
+            char ch=password.charAt(i);
+            if((ch >= 'A') && (ch <= 'Z')){
+                upperLetter = true;
+            }
+            else if((ch >= 'a') && (ch <= 'z')){
+                lowerLetter = true;
+            }
+            else if((ch >= '0') && (ch <= '9')){
+                digit = true;
+            }
+            else specialChar = true;
+        }
+
+        if(upperLetter && lowerLetter && digit && specialChar){
+            return true;
+        }
+        return false;
     }
 }
